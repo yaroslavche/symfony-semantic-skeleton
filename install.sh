b36b6376202e8c145ff3b7ce91330a2b37310ef9
yaroslavche <yaroslav429@gmail.com>
2018-06-23 00:57:28 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
+#!/bin/bash
+
 composer install
-bin/console doctrine:database:create
-bin/console doctrine:schema:create
-bin/console doctrine:fixtures:load
+
 yarn install
 npm install semantic-ui
 (cd semantic && gulp build)
 yarn run encore dev
+
+withDatabase=false
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --db*)
+            withDatabase=true
+            ;;
+        --test*)
+            test="${1#*=}"
+            ;;
+        *)
+            printf "Error: Invalid argument \"$1\"\n"
+            exit 1
+  esac
+  shift
+done
+
+if [ "$withDatabase" = true ] ; then
+    bin/console doctrine:database:create
+    bin/console doctrine:schema:create
+    bin/console doctrine:fixtures:load
+fi
