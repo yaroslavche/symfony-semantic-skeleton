e60c1f4bd7dbdf0729abcb728617f1a9f2314a9b
yaroslavche <yaroslav429@gmail.com>
2018-06-22 23:29:40 +0300

    fix encore build path

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     // the project directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('public/assets/')
     // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
+    .setPublicPath('/assets')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
