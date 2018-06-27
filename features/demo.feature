051ed91a9a8937f9e041ed5e0cc68729b1793673
yaroslavche <yaroslav429@gmail.com>
2018-06-27 14:32:45 +0300

    + test user login, --test arg

--- features/demo.feature
+++ features/demo.feature
@@ -3,10 +3,18 @@
 # http://behat.org/en/latest/quick_start.html
 
 Feature:
-  In order to prove that the Behat Symfony extension is correctly installed
-  As a user
-  I want to have a demo scenario
+    In order to prove that the Behat Symfony extension is correctly installed
+    As a user
+    I want to have a demo scenario
 
-  Scenario: It receives a response from Symfony's kernel
-    When a demo scenario sends a request to "/"
-    Then the response should be received
+    Scenario: It receives a response from Symfony's kernel
+        When a demo scenario sends a request to "/"
+        Then the response should be received
+
+    Scenario: A user can login
+        Given I am on "http://localhost:8000/admin"
+        And fill in "username" with "admin"
+        And fill in "password" with "123"
+        And press "_submit"
+        # And print last response
+        Then I should see "admin"
--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+withDatabase=false
+withTest=false
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --db*)
+            withDatabase=true
+            ;;
+        --test*)
+            withTest=true
+            ;;
+        *)
+            printf "Error: Invalid argument \"$1\"\n"
+            exit 1
+    esac
+    shift
+done
+
+
 composer install
 
 yarn install
@@ -7,21 +25,14 @@ npm install semantic-ui
 (cd semantic && gulp build)
 yarn run encore dev
 
-withDatabase=false
-while [ $# -gt 0 ]; do
-    case "$1" in
-        --db*)
-            withDatabase=true
-            ;;
-        *)
-            printf "Error: Invalid argument \"$1\"\n"
-            exit 1
-  esac
-  shift
-done
-
 if [ "$withDatabase" = true ] ; then
     bin/console doctrine:database:create
     bin/console doctrine:schema:create
     bin/console doctrine:fixtures:load
+fi
+
+if [ "$withTest" = true ] ; then
+    bin/console server:start
+    vendor/bin/behat
+    bin/console server:stop
 fi
