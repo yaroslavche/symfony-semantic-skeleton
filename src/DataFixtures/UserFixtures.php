c60544b799593aa2312ff24df29f8557e3902033
yaroslavche <yaroslav429@gmail.com>
2018-11-01 20:56:52 +0200

    Update locks

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,27 +4,32 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
 
-class UserFixtures extends Fixture implements ContainerAwareInterface
+class UserFixtures extends Fixture
 {
-    private $container;
+    /**
+     * @var UserManagerInterface $userManager
+     */
+    private $userManager;
 
-    public function setContainer(?ContainerInterface $container = null)
+    /**
+     * UserFixtures constructor.
+     * @param UserManagerInterface $userManager
+     */
+    public function __construct(UserManagerInterface $userManager)
     {
-        $this->container = $container;
+        $this->userManager = $userManager;
     }
 
     public function load(ObjectManager $manager)
     {
-        $userManager = $this->container->get('fos_user.user_manager');
-        $user = $userManager->createUser();
+        $user = $this->userManager->createUser();
         $user->setUsername('admin');
         $user->setEmail('admin@test.dev');
         $user->setPlainPassword('123');
         $user->setEnabled(true);
         $user->setRoles(array('ROLE_SUPER_ADMIN'));
-        $userManager->updateUser($user, true);
+        $this->userManager->updateUser($user, true);
     }
 }
